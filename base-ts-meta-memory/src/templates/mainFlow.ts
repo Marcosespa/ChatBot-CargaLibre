7033c57cc92bb708981811994376c73028790223
Marcosespa <marcosespa24@gmail.com>
2025-01-02 18:16:40 -0500

    Creado el Sheets

--- base-ts-meta-memory/src/templates/mainFlow.ts
+++ base-ts-meta-memory/src/templates/mainFlow.ts
@@ -1,6 +1,6 @@
 import { addKeyword, EVENTS } from "@builderbot/bot";
 
 const mainFlow = addKeyword(EVENTS.WELCOME)
-  .addAnswer('👋 Hello welcome to this *Chatbot*');
+  .addAnswer('👋 Hello welcome to this MARCVOCC *Chatbot*');
 
 export { mainFlow };
